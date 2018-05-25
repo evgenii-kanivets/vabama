33c7128ba828303d84f78553962b841647c62ed4
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2018-05-26 00:36:50 +0200

    Added images.

--- app/src/main/java/com/tech_crunch/hackaton/vabama/data/DataLoader.kt
+++ app/src/main/java/com/tech_crunch/hackaton/vabama/data/DataLoader.kt
@@ -10,16 +10,16 @@ object DataLoader {
 
     fun loadData() {
         val carDao = VbmApp.app.carDao
-        carDao.insert(Car(1, "MD1012TZ", "Renaud Megan", R.drawable.car1, OK_STATUS))
-        carDao.insert(Car(2, "MD2012TZ", "Ford Focus", R.drawable.car1, NEED_CLEANING_STATUS))
-        carDao.insert(Car(3, "MD3012TZ", "Suzuki Megan", R.drawable.car1, NEED_REPAIR_STATUS))
-        carDao.insert(Car(4, "MD4012TZ", "Citroen C1", R.drawable.car1, OK_STATUS))
-        carDao.insert(Car(5, "MD5012TZ", "Renaud Forza", R.drawable.car1, OK_STATUS))
-        carDao.insert(Car(6, "MD6012TZ", "Renaud PO", R.drawable.car1, NEED_REPAIR_STATUS))
-        carDao.insert(Car(7, "MD7012TZ", "Ford C1", R.drawable.car1, NEED_CLEANING_STATUS))
-        carDao.insert(Car(8, "MD6212TZ", "Ferrari Megan", R.drawable.car1, NEED_PAINTING_STATUS))
-        carDao.insert(Car(9, "MD7112TZ", "Pojo Megan", R.drawable.car1, NEED_PAINTING_STATUS))
-        carDao.insert(Car(10, "TR7112TZ", "Chloe Megan", R.drawable.car1, NEED_PAINTING_STATUS))
+        carDao.insert(Car(1, "ZY1012TZ", "Chrysler 300s", R.drawable.car1, OK_STATUS))
+        carDao.insert(Car(2, "MD2012TZ", "Chrysler 300 Limited Awd", R.drawable.car2, NEED_CLEANING_STATUS))
+        carDao.insert(Car(3, "MD3012TZ", "Chrysler 300s black", R.drawable.car3, NEED_REPAIR_STATUS))
+        carDao.insert(Car(4, "MD4012TZ", "Chrysler 300s", R.drawable.car4, OK_STATUS))
+        carDao.insert(Car(5, "MD5012TZ", "Toyota Prius", R.drawable.car5, OK_STATUS))
+        carDao.insert(Car(6, "MD6012TZ", "Toyota Prius Blue", R.drawable.car6, NEED_REPAIR_STATUS))
+        carDao.insert(Car(7, "MD7012TZ", "toyota Prius White", R.drawable.car7, NEED_CLEANING_STATUS))
+        carDao.insert(Car(8, "MD6212TZ", "Mercedes Benz C-Class Coupe", R.drawable.car8, NEED_PAINTING_STATUS))
+        carDao.insert(Car(9, "MD7112TZ", "Mercedes Benz C-Class Coupe", R.drawable.car9, NEED_PAINTING_STATUS))
+        carDao.insert(Car(10, "TR7112TZ", "Mercedes Benz C-Class White", R.drawable.car10, NEED_PAINTING_STATUS))
     }
 
 }
